e9c8d0b13281ffe70d4fb53204e88fb6237cc91c
Greg Pfaff <greg.pfaff@sapientrazorfish.com>
2018-10-28 21:15:10 -0500

    adding server

---
+++ buildScripts/server.js
@@ -0,0 +1,19 @@
+// import express from 'express';
+// import path from 'path';
+const express = require('express');
+const path = require('path');
+
+const port = 5555;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './src/index.html'));
+});
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log('Error', error)
+  } else {
+    console.log(`pfJS listening on port ${port}!`)
+  }
+});

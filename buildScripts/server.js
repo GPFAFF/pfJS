9f4e964adcae98c44164c727e336772301964650
Greg Pfaff <greg.pfaff@sapientrazorfish.com>
2018-10-31 21:45:05 -0500

    adding adding mock api data

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -16,6 +16,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', (req, res) => {
+  res.json([
+    {"id": 1, "firstName": "Greg", "lastName": "Pfaff", "email": "greg.pfaff1@gmail.com"},
+    {"id": 2, "firstName": "Greg2", "lastName": "Pfaff2", "email": "greg.pfaff2@gmail.com"},
+    {"id": 3, "firstName": "Greg3", "lastName": "Pfaff3", "email": "greg.pfaff3@gmail.com"}
+  ]);
+});
+
 app.listen(port, (error) => {
   /* eslint-disable no-console */
   (error) ? console.log('Error', error) : console.log(`pfJS listening on port ${port}!`);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
 import './index.scss';
+import { getUsers, deleteUser } from './api/userApi';
 
-/* eslint-disable no-console */
-console.log('I am loading webpack');
+
+getUsers().then(result => {
+  const container = document.querySelector('.container');
+  let userBody = '';
+
+  result.map(user => {
+    userBody += `
+      <div class='row'>
+        <a class='delete-user' href='#' data-id=${user.id}>Delete</a>
+        <span>${user.id}</span>
+        <span>${user.firstName}</span>
+        <span>${user.lastName}</span>
+        <span>${user.email}</span>
+      </div>
+    `
+  });
+
+  container.innerHTML = userBody;
+
+  const deleteLinks = document.querySelectorAll('.delete-user');
+
+  console.log(deleteLinks);
+
+  deleteLinks.forEach((link) => {
+    link.addEventListener('click', (event) => {
+      console.log('click');
+      const current = event.target;
+      event.preventDefault();
+      deleteUser(current.attributes['data-id'].value);
+      const row = current.parentNode;
+      row.parentNode.removeChild(row);
+    })
+  })
+})
